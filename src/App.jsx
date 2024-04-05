fe98ad47916847e7affaafac13d4fd01334ec028
vikus <victtorandrade7mh@gmail.com>
2024-04-04 22:41:39 -0500

    feat: add component title and install lint
    Install husky and create precommit hook to use lint.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+      <>
+        <div>Victor Rodriguez</div>
+        <div><img id="profile" src="https://avatars.githubusercontent.com/u/119013345?v=4"/></div>
+      </>
+
+    </div>
+  )
+}
+
+export default App
